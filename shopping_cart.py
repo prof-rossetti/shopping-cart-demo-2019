95143d57f4e1fc1f73890d208286f4cebea59e34
Mike Rossetti <s2t2@users.noreply.github.com>
2019-02-01 16:55:48 -0500

    Lookup product given identifier

--- shopping_cart.py
+++ shopping_cart.py
@@ -28,8 +28,9 @@ products = [
 #
 
 selected_id = input("Please input a product identifier: ") #> "9" (string)
-print(selected_id)
-print(type(selected_id))
+matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+matching_product = matching_products[0]
+print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
 
 #
 # INFO DISPLAY / OUTPUT

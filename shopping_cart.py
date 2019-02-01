4d477cd5af2fea03cb78ba7187fb032214d168c8
Mike Rossetti <s2t2@users.noreply.github.com>
2019-02-01 17:05:10 -0500

    Iteratively prompt user for inputs and handle DONE

--- shopping_cart.py
+++ shopping_cart.py
@@ -27,10 +27,14 @@ products = [
 # INFO CAPTURE / INPUT
 #
 
-selected_id = input("Please input a product identifier: ") #> "9" (string)
-matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-matching_product = matching_products[0]
-print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+while True:
+    selected_id = input("Please input a product identifier: ") #> "9" (string)
+    if selected_id == "DONE":
+        break
+    else:
+        matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+        matching_product = matching_products[0]
+        print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
 
 #
 # INFO DISPLAY / OUTPUT

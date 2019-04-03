9085d59d607b04b6775d0cd818f0d551203635c8
Mike Rossetti <s2t2@users.noreply.github.com>
2019-04-03 13:14:35 -0400

    Test edge-case scenarios

--- shopping_cart_test.py
+++ shopping_cart_test.py
@@ -2,6 +2,14 @@
 from shopping_cart import to_usd
 
 def test_to_usd():
-    price = 4.5
-    price_usd = to_usd(price)
-    assert price_usd == "$4.50"
+    # it should apply USD formatting
+    assert to_usd(4.50) == "$4.50"
+
+    # it should display two decimal places
+    assert to_usd(4.5) == "$4.50"
+
+    # it should round to two places
+    assert to_usd(4.55555) == "$4.56"
+
+    # it should display thousands separators
+    assert to_usd(1234567890.5555555) == "$1,234,567,890.56"
